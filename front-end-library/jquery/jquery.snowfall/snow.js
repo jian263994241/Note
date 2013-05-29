0217c7dbd4764b39c64a0b7323f03933e2bce0c3
ukey <ukey.li@sh.leoburnet.com>
2013-05-29 17:27:42 +0800

    ukey

--- front-end-library/jquery/jquery.snowfall/snow.js
+++ front-end-library/jquery/jquery.snowfall/snow.js
@@ -1,10 +1,14 @@
-define(function(require,exports,module){
-    require('snowfall');
-
-    function flower(){
+define(['snowfall'],function(){
+    var flower = {};
+    var ctx = $('#snowContainer');
+    flower.ctx = ctx;
+    flower.start = function(){
+        ctx.height($(document).height());
         var flower_path = '/Public/Asserts/images/flower/';
         $('#snowContainer').snowfall({image :[flower_path+"bloomsom2.png",flower_path+"bloomsom3.png",flower_path+"bloomsom4.png",flower_path+"bloomsom5.png",flower_path+"bloomsom6.png",flower_path+"bloomsom7.png",flower_path+"bloomsom8.png"], minSize: 60, maxSize:120, flakeCount:20});
     }
-    flower();
-    exports = flower;
+    flower.clear = function(){
+        $('#snowContainer').snowfall('clear');
+    }
+    return flower;
 });
