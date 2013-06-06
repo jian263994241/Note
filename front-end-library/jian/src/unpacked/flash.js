5f2efdacf9f818637bbe105b6b592bbd4d60cfac
ukey <ukey.li@sh.leoburnet.com>
2013-06-06 16:33:01 +0800

    ukey

---
+++ front-end-library/jian/src/unpacked/flash.js
@@ -0,0 +1,11 @@
+define(function(){
+    function swf(d,a,b,eid){
+        if (navigator.appName.indexOf("Microsoft") != -1) {
+            return '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="'+a+'" height="'+b+'" id="'+eid+'" align="middle"><param name="movie" value="'+d+'" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" /><param name="play" value="true" /><param name="loop" value="true" /><param name="wmode" value="transparent" /><param name="scale" value="showall" /><param name="menu" value="true" /><param name="devicefont" value="false" /><param name="salign" value="" /><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /></object>';
+        }else{
+            return '<embed src="'+d+'" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" id="'+eid+'" name="'+eid+'" bgcolor="#000000" width="'+a+'" height="'+b+'" allowscriptaccess="always" quality="High" wmode="transparent" allowFullScreen="true"></embed>';
+        }
+    };
+
+    return swf;
+})
